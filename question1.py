9b56c14479af43080ec6df6aefba5022a0bf7677
the-hk <conjointchill@gmail.com>
2021-01-05 03:48:56 +0300

    second commint with end up first question

--- question1.py
+++ question1.py
@@ -1,36 +1,56 @@
 import numpy as np
-import math
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt 
+import PIL
+import cv2
+from PIL import Image, ImageFilter
 
-filename=0;
-
-#im,height,width
+#,im,height,width
 class ImageFilt:
-    
-    def __init__(self,filename,im,height,width):
+    def __init__(self,filename):
         self.filename = filename                           
-        self.im = im
-        self.height = height
-        self.width = width
-        print("im in init")
-        
-    def load_im(self,filenameame):
-       # A method named load_im() that loads the image, sets height and width and
-       #returns true if the file exists; else returns false and prints a warning that says
-       #“Cannot find the image file”.
-        try:
-            f = open(filename)
+        self.im = 0
+        self.height = 0
+        self.width = 0
+    
+    def load_im(self):
+
+        try: 
+            self.im = cv2.imread(self.filename)
+            #print(self.im)
+            self.height, self.width, channels = self.im.shape
+            print("width of image",self.width)
+            print("height of image",self.height)
             return True
-            
+        
         except:
-            print("sa")
-            
-    def filter_im(f):
+            print("Cannot find the image file")
+          
+                    
+    def filter_im(self,f):
         #The method filter_im(f) that filters the image using a blur filter if f=0, or using a
         #sharpening filter if f =1.
+        if f==1:
+            kernel = np.ones((5,5),np.float32)/25
+            dst = cv2.filter2D(self.im,-1,kernel)
+
+            plt.subplot(121),plt.imshow(self.im),plt.title('Original')
+            plt.xticks([]), plt.yticks([])
+            plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+            plt.xticks([]), plt.yticks([])
+            plt.show()
+            
+        elif f==0:
+            pass
         
     def plot_im():
         #The method plot_im() that plots the original and filtered images as subplots in a
         #single figure window.
+        pass
         
-   
-                  
+        
+path = '/home/hk/Desktop/bionluk/sa.jpeg'
+image = ImageFilt(path)
+image.load_im()
+image.filter_im(1)
+
